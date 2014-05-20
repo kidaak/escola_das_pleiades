d67acbc8a3d40be104b2e23c45811c1dd4387c52
Andre Pinho <andresouzapinho@gmail.com>
2014-05-20 15:49:27 -0300

    Try to fix locale problem

--- config/application.rb
+++ config/application.rb
@@ -33,8 +33,10 @@ module EscolaDasPleiades
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.enforce_available_locales = false
     config.i18n.default_locale = :'pt-BR'
     config.i18n.locale = :'pt-BR'
-    config.i18n.available_locales = %w(pt-BR)
+    config.i18n.available_locales = ['pt-BR']
   end
 end
