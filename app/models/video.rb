10487ab2513f76e3696863119db7775ee896a2a0
Andre Pinho <andresouzapinho@gmail.com>
2014-05-24 19:43:21 -0300

    Create method that only lets one home_page video to be true

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,11 +2,23 @@ class Video < ActiveRecord::Base
   validates :title, :url, presence: true
   validates :url, url: true
 
+  before_save :highlight
+
   def self.home
     where("home_page is TRUE").first
   end
 
   def code
     self.url[-11,11]
+  end
+
+  def highlight
+    if self.home_page
+      video = Video.where("home_page is TRUE").first
+      if video.present?
+        video.home_page = nil
+        video.save
+      end
+    end
   end
 end
