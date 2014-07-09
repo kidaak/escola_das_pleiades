d25a71373d96585e48d303edf9bc269291c3e904
Andre Pinho <andresouzapinho@gmail.com>
2014-07-08 23:01:36 -0300

    Add pagination to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all.by_relevance
+    @events = Event.all.by_relevance.page(params[:page]).per(12)
   end
   def show
     @event = Event.find(params[:id])
