e5b0a5afef7cd0c9071176b3b37f9220692a9e41
Andre Pinho <andresouzapinho@gmail.com>
2014-07-08 22:51:00 -0300

    Fix order on album index

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,6 @@
 class AlbumsController < ApplicationController
   def index
-    @albums = Album.all
+    @albums = Album.all.order(updated_at: :desc, created_at: :desc)
   end
   def show
     @album = Album.find(params[:id])
