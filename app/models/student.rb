fd887f0aff878d902aee7095eda43ebcf91db050
Andre Pinho <andresouzapinho@gmail.com>
2014-08-07 10:06:10 -0300

    Add validation to student email

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,7 @@ class Student < ActiveRecord::Base
   # :confirmable, :rememberable, :recoverable, :lockable, :registerable,
   # :timeoutable, :validatable and :omniauthable
   validates :email, :password, presence: true
+  validates :email, uniqueness: {case_sensitive: false}
   devise :database_authenticatable, :trackable
   has_many :uploads
   accepts_nested_attributes_for :uploads, :allow_destroy => true
