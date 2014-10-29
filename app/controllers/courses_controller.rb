f037089f3c5954da6fce59631b1c40ab3b476070
Andre Pinho <andresouzapinho@gmail.com>
2014-10-29 20:43:54 -0200

    Fixes create_contact method

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,4 +10,20 @@ class CoursesController < ApplicationController
     @course = Course.find(params[:id])
     @contact = Contact.new
   end
+
+  def create_contact
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to root_path, notice: 'Obrigado. Você receberá instruções sobre como continuar em seu email'
+    else
+      @course = Course.find(params[:id])
+      render :subscribe
+    end
+  end
+
+  def contact_params
+    params.require(:contact).permit(:full_name, :email, :cpf_cnpj, :zip_code,
+                                    :address, :neighborhood, :city, :state,
+                                    :course_id)
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,20 +12,4 @@ class PagesController < ApplicationController
   def contact
   end
 
-  def create_contact
-    @contact = Contact.new(contact_params)
-    if @contact.save
-      redirect_to root_path, notice: 'Obrigado. Você receberá instruções sobre como continuar em seu email'
-    else
-      render :contact
-    end
-  end
-
-  private
-
-  def contact_params
-    params.require(:contact).permit(:full_name, :email, :cpf_cnpj, :zip_code,
-                                    :address, :neighborhood, :city, :state,
-                                    :course_id)
-  end
 end
